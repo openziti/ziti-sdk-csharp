22db250eb5c66389430de5b71b3828169337334e
dovholuknf <46322585+dovholuknf@users.noreply.github.com>
2021-04-21 18:16:09 -0400

    attempt to push to nuget from github

--- ZitiNativeApiForDotnetCore/msvc-build.bat
+++ ZitiNativeApiForDotnetCore/msvc-build.bat
@@ -1,8 +1,9 @@
 @echo off
+SET ZITI_SDK_C_BRANCH=0.22.4
+
 REM a stupid env var JUST to allow a space to be set into an environment variable using substring...
 set ZITI_SPACES=:   :
 
-SET ZITI_SDK_C_BRANCH=main
 if "%ZITI_SDK_C_BRANCH%"=="" (
     echo ZITI_SDK_C_BRANCH is not set - ZITI_SDK_C_BRANCH_CMD will be empty
     SET ZITI_SDK_C_BRANCH_CMD=%ZITI_SPACES:~2,1%
@@ -17,7 +18,7 @@ set CSDK_HOME=%~dp0
 cd /d %CSDK_HOME%
 
 set cmake=C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\IDE\CommonExtensions\Microsoft\CMake\CMake\bin\cmake.exe
-set BUILDFOLDER=%CSDK_HOME%build
+set BUILDFOLDER=%CSDK_HOME%build-win
 
 mkdir %BUILDFOLDER% 2> NUL
 mkdir %BUILDFOLDER%\x86 2> NUL
@@ -30,7 +31,7 @@ popd
 
 pushd %BUILDFOLDER%\x64
 REM cmake ..\.. -G "Visual Studio 16 2019" -A x64 -DCMAKE_INSTALL_INCLUDEDIR=include -DCMAKE_INSTALL_LIBDIR=lib
-"%cmake%" -S %CSDK_HOME% -B %BUILDFOLDER%\x64 -G "Visual Studio 16 2019" -A Win32 -DCMAKE_INSTALL_INCLUDEDIR=include -DCMAKE_INSTALL_LIBDIR=lib %ZITI_SDK_C_BRANCH_CMD%
+"%cmake%" -S %CSDK_HOME% -B %BUILDFOLDER%\x64 -G "Visual Studio 16 2019" -A x64 -DCMAKE_INSTALL_INCLUDEDIR=include -DCMAKE_INSTALL_LIBDIR=lib %ZITI_SDK_C_BRANCH_CMD%
 popd
 
 ECHO Build from cmake using: 
