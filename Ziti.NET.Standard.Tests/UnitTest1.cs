f5d90a469bc79bb1c851fb7590616e2e30c4acfa
dovholuknf <46322585+dovholuknf@users.noreply.github.com>
2021-04-20 23:20:47 -0400

    wip commit 2 apr 20

---
+++ Ziti.NET.Standard.Tests/UnitTest1.cs
@@ -0,0 +1,19 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using OpenZiti;
+using System;
+using System.Threading.Tasks;
+
+namespace Ziti.NET.Standard.Tests {
+    [TestClass]
+    public class UnitTest1 {
+        [TestMethod]
+        public void TestMethod1() {
+            
+        }
+
+        private void afterEnroll(ZitiStatus status) {
+            throw new NotImplementedException();
+        }
+    }
+}
