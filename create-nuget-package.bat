9777c7af1bcb0390cc2df8465f5eaae85f2f5f09
dovholuknf <46322585+dovholuknf@users.noreply.github.com>
2021-07-21 19:28:39 -0400

    add icon, nuspec files, bat files for addition nuget packaging

---
+++ create-nuget-package.bat
@@ -0,0 +1,18 @@
+@echo off
+
+dotnet build ziti.nuget.sln --configuration Release /p:Platform=x86
+dotnet build ziti.nuget.sln --configuration Release /p:Platform=x64
+
+REM :point-down: does not create a nuget package which is compatible with "AnyCPU" somehow?
+REM dotnet build ziti.nuget.sln --configuration Release
+
+mkdir Ziti.NET.Standard\bin\Release\netstandard2.1\
+
+REM Thanks to https://msicc.net/how-to-create-a-multi-architecture-nuget-package-from-a-uwp-class-library/
+REM for the corflags tip
+echo "copying x86 dll and removing removing the 32Bit-flag to be AnyCPU compatible"
+cp Ziti.NET.Standard\bin\x86\Release\netstandard2.1\Ziti.NET.Standard.dll Ziti.NET.Standard\bin\Release\netstandard2.1\Ziti.NET.Standard.dll
+cp Ziti.NET.Standard\bin\x86\Release\netstandard2.1\Ziti.NET.Standard.pdb Ziti.NET.Standard\bin\Release\netstandard2.1\Ziti.NET.Standard.pdb
+corflags.exe /32bitreq- Ziti.NET.Standard\bin\Release\netstandard2.1\Ziti.NET.Standard.dll
+
+dotnet build Ziti.NuGet.sln /t:PackNuspec
