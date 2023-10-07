4e04fa4c9dcd0963470cde7269caa211ebcb23e4
dovholuknf <46322585+dovholuknf@users.noreply.github.com>
2023-10-06 23:35:24 -0400

    update alignment checking for 0.35. make it easier to figure out the next time

--- ZitiNativeApiForDotnetCore/includes/ziti4dotnet.h
+++ ZitiNativeApiForDotnetCore/includes/ziti4dotnet.h
@@ -20,46 +20,104 @@
 extern "C" {
 #endif
 
-#define FIELD_WITH_CHECK(FIELD_TYPE, FIELD_NAME) FIELD_TYPE FIELD_NAME; \
-                    const char* FIELD_NAME##_checksum;                  \
-                    uint32_t FIELD_NAME##_size;                         \
-                    uint32_t FIELD_NAME##_offset
-typedef struct ziti_types_s {
-    FIELD_WITH_CHECK(uint32_t, total_size);
+#define ALIGNMENT_CHECK(FIELD_TYPE)             \
+typedef struct ziti_alignment_check_##FIELD_TYPE##_s {       \
+                    uint32_t offset;                        \
+                    uint32_t size;                          \
+                    const char* checksum;                   \
+} ziti_alignment_check_##FIELD_TYPE
+#define ALIGNMENT_FIELD(FIELD_TYPE, FIELD_NAME) \
+  ziti_alignment_check_##FIELD_TYPE FIELD_NAME
 
-    FIELD_WITH_CHECK(ziti_auth_query_mfa, ziti_auth_query_mfa);
-    FIELD_WITH_CHECK(ziti_id_cfg, ziti_id_cfg);
-    FIELD_WITH_CHECK(ziti_config, ziti_config);
-    FIELD_WITH_CHECK(api_path, api_path);
-    FIELD_WITH_CHECK(ziti_api_versions, ziti_api_versions);
-    FIELD_WITH_CHECK(ziti_version, ziti_version);
-    FIELD_WITH_CHECK(ziti_identity, ziti_identity);
-    FIELD_WITH_CHECK(ziti_process, ziti_process);
-    FIELD_WITH_CHECK(ziti_posture_query, ziti_posture_query);
-    FIELD_WITH_CHECK(ziti_posture_query_set, ziti_posture_query_set);
-    FIELD_WITH_CHECK(ziti_session_type, ziti_session_type);
-    FIELD_WITH_CHECK(ziti_service, ziti_service);
-    FIELD_WITH_CHECK(ziti_address, ziti_address_host);
-    FIELD_WITH_CHECK(ziti_address, ziti_address_cidr);
-    FIELD_WITH_CHECK(ziti_client_cfg_v1, ziti_client_cfg_v1);
-    FIELD_WITH_CHECK(ziti_intercept_cfg_v1, ziti_intercept_cfg_v1);
-    FIELD_WITH_CHECK(ziti_server_cfg_v1, ziti_server_cfg_v1);
-    FIELD_WITH_CHECK(ziti_listen_options, ziti_listen_options);
-    FIELD_WITH_CHECK(ziti_host_cfg_v1, ziti_host_cfg_v1);
-    FIELD_WITH_CHECK(ziti_host_cfg_v2, ziti_host_cfg_v2);
-    FIELD_WITH_CHECK(ziti_mfa_enrollment, ziti_mfa_enrollment);
-    FIELD_WITH_CHECK(ziti_port_range, ziti_port_range);
-    FIELD_WITH_CHECK(ziti_options, ziti_options);
+ALIGNMENT_CHECK(ziti_auth_query_mfa);
+ALIGNMENT_CHECK(ziti_id_cfg);
+ALIGNMENT_CHECK(ziti_config);
+ALIGNMENT_CHECK(api_path);
+ALIGNMENT_CHECK(ziti_api_versions);
+ALIGNMENT_CHECK(ziti_version);
+ALIGNMENT_CHECK(ziti_identity);
+ALIGNMENT_CHECK(ziti_process);
+ALIGNMENT_CHECK(ziti_posture_query);
+ALIGNMENT_CHECK(ziti_posture_query_set);
+ALIGNMENT_CHECK(ziti_session_type);
+ALIGNMENT_CHECK(ziti_service);
+ALIGNMENT_CHECK(ziti_address);
+ALIGNMENT_CHECK(ziti_client_cfg_v1);
+ALIGNMENT_CHECK(ziti_intercept_cfg_v1);
+ALIGNMENT_CHECK(ziti_server_cfg_v1);
+ALIGNMENT_CHECK(ziti_listen_options);
+ALIGNMENT_CHECK(ziti_host_cfg_v1);
+ALIGNMENT_CHECK(ziti_host_cfg_v2);
+ALIGNMENT_CHECK(ziti_mfa_enrollment);
+ALIGNMENT_CHECK(ziti_port_range);
+ALIGNMENT_CHECK(ziti_options);
+ALIGNMENT_CHECK(ziti_event_t);
 
-    //events
-    FIELD_WITH_CHECK(ziti_event_t, ziti_context_event);
-    FIELD_WITH_CHECK(ziti_event_t, ziti_router_event);
-    FIELD_WITH_CHECK(ziti_event_t, ziti_service_event);
-    FIELD_WITH_CHECK(ziti_event_t, ziti_mfa_auth_event);
-    FIELD_WITH_CHECK(ziti_event_t, ziti_api_event);
+#define ALIGNMENT_DATA(FIELD_TYPE, FIELD_NAME) \
+  FIELD_TYPE FIELD_NAME##_data
 
+typedef struct ziti_types_v2_s {
+    uint32_t size; //declare as a pointer but use as a value
+    // declare all the alignements: offset, size, checksum
+    ALIGNMENT_FIELD(ziti_auth_query_mfa, ziti_auth_query_mfa);
+    ALIGNMENT_FIELD(ziti_id_cfg, ziti_id_cfg);
+    ALIGNMENT_FIELD(ziti_config, ziti_config);
+    ALIGNMENT_FIELD(api_path, api_path);
+    ALIGNMENT_FIELD(ziti_api_versions, ziti_api_versions);
+    ALIGNMENT_FIELD(ziti_version, ziti_version);
+    ALIGNMENT_FIELD(ziti_identity, ziti_identity);
+    ALIGNMENT_FIELD(ziti_process, ziti_process);
+    ALIGNMENT_FIELD(ziti_posture_query, ziti_posture_query);
+    ALIGNMENT_FIELD(ziti_posture_query_set, ziti_posture_query_set);
+    ALIGNMENT_FIELD(ziti_session_type, ziti_session_type);
+    ALIGNMENT_FIELD(ziti_service, ziti_service);
+    ALIGNMENT_FIELD(ziti_address, ziti_address_host);
+    ALIGNMENT_FIELD(ziti_address, ziti_address_cidr);
+    ALIGNMENT_FIELD(ziti_client_cfg_v1, ziti_client_cfg_v1);
+    ALIGNMENT_FIELD(ziti_intercept_cfg_v1, ziti_intercept_cfg_v1);
+    ALIGNMENT_FIELD(ziti_server_cfg_v1, ziti_server_cfg_v1);
+    ALIGNMENT_FIELD(ziti_listen_options, ziti_listen_options);
+    ALIGNMENT_FIELD(ziti_host_cfg_v1, ziti_host_cfg_v1);
+    ALIGNMENT_FIELD(ziti_host_cfg_v2, ziti_host_cfg_v2);
+    ALIGNMENT_FIELD(ziti_mfa_enrollment, ziti_mfa_enrollment);
+    ALIGNMENT_FIELD(ziti_port_range, ziti_port_range);
+    ALIGNMENT_FIELD(ziti_options, ziti_options);
+    ALIGNMENT_FIELD(ziti_event_t, ziti_context_event);
+    ALIGNMENT_FIELD(ziti_event_t, ziti_router_event);
+    ALIGNMENT_FIELD(ziti_event_t, ziti_service_event);
+    ALIGNMENT_FIELD(ziti_event_t, ziti_mfa_auth_event);
+    ALIGNMENT_FIELD(ziti_event_t, ziti_api_event);
 
-} ziti_types_t;
+    // now declare "_data" elemnets - the __ACTUAL__ structs
+    ALIGNMENT_DATA(ziti_auth_query_mfa, ziti_auth_query_mfa);
+    ALIGNMENT_DATA(ziti_id_cfg, ziti_id_cfg);
+    ALIGNMENT_DATA(ziti_config, ziti_config);
+    ALIGNMENT_DATA(api_path, api_path);
+    ALIGNMENT_DATA(ziti_api_versions, ziti_api_versions);
+    ALIGNMENT_DATA(ziti_version, ziti_version);
+    ALIGNMENT_DATA(ziti_identity, ziti_identity);
+    ALIGNMENT_DATA(ziti_process, ziti_process);
+    ALIGNMENT_DATA(ziti_posture_query, ziti_posture_query);
+    ALIGNMENT_DATA(ziti_posture_query_set, ziti_posture_query_set);
+    ALIGNMENT_DATA(ziti_session_type, ziti_session_type);
+    ALIGNMENT_DATA(ziti_service, ziti_service);
+    ALIGNMENT_DATA(ziti_address, ziti_address_host);
+    ALIGNMENT_DATA(ziti_address, ziti_address_cidr);
+    ALIGNMENT_DATA(ziti_client_cfg_v1, ziti_client_cfg_v1);
+    ALIGNMENT_DATA(ziti_intercept_cfg_v1, ziti_intercept_cfg_v1);
+    ALIGNMENT_DATA(ziti_server_cfg_v1, ziti_server_cfg_v1);
+    ALIGNMENT_DATA(ziti_listen_options, ziti_listen_options);
+    ALIGNMENT_DATA(ziti_host_cfg_v1, ziti_host_cfg_v1);
+    ALIGNMENT_DATA(ziti_host_cfg_v2, ziti_host_cfg_v2);
+    ALIGNMENT_DATA(ziti_mfa_enrollment, ziti_mfa_enrollment);
+    ALIGNMENT_DATA(ziti_port_range, ziti_port_range);
+    ALIGNMENT_DATA(ziti_options, ziti_options);
+    ALIGNMENT_DATA(ziti_event_t, ziti_context_event);
+    ALIGNMENT_DATA(ziti_event_t, ziti_router_event);
+    ALIGNMENT_DATA(ziti_event_t, ziti_service_event);
+    ALIGNMENT_DATA(ziti_event_t, ziti_mfa_auth_event);
+    ALIGNMENT_DATA(ziti_event_t, ziti_api_event);
+} ziti_types_v2;
 
 Z4D_API int z4d_ziti_close(ziti_connection con);
 Z4D_API int z4d_uv_run(void* loop);
@@ -78,7 +136,7 @@ Z4D_API void z4d_free_char_array(char **a, int size);
 Z4D_API void z4d_ziti_dump_log(ziti_context ztx);
 Z4D_API void z4d_ziti_dump_file(ziti_context ztx, const char* outputFile);
 
-Z4D_API ziti_types_t* z4d_struct_test();
+Z4D_API ziti_types_v2* z4d_struct_test();
 Z4D_API ziti_posture_query* z4d_ziti_posture_query();
 
 typedef void (*z4d_cb)(void* context);
--- ZitiNativeApiForDotnetCore/library/zitistructalignment.c
+++ ZitiNativeApiForDotnetCore/library/zitistructalignment.c
@@ -3,8 +3,6 @@
 
 int main() {
     printf("\noffset output begins\n");
-    ziti_types_t* rtn = z4d_struct_test();
-    printf(rtn->ziti_posture_query_set_checksum);
-    char* r = model_map_get(&rtn->ziti_service.config, "config_key1");
+    ziti_types_v2* rtn = z4d_struct_test();
     printf("\noffset output complete\n\n");
 }
